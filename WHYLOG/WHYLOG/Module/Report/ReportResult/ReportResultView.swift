583ae43188c34cf6952562e834e416f4c3c5872a
guingguing <kimjinseo0402@gmail.com>
2025-12-19 04:02:36 +0900

    [Feat] 리포트 결과 뷰 UI 구현

---
+++ WHYLOG/WHYLOG/Module/Report/ReportResult/ReportResultView.swift
@@ -0,0 +1,184 @@
+//
+//  ReportResultView.swift
+//  WHYLOG
+//
+//  Created by 김진서 on 12/19/25.
+//
+
+import SwiftUI
+
+struct ReportResultView: View {
+    @Environment(\.dismiss) private var dismiss
+
+    var body: some View {
+        ZStack {
+            // Background
+            Color(.baseCoral)
+                .ignoresSafeArea()
+
+            VStack(spacing: 0) {
+                navigationBar
+
+                ScrollView {
+                    topContent
+                    graphBox
+                    middleContent
+                    writing
+                }
+
+                confirmButton
+            }
+            .padding(.horizontal, 20)
+        }
+        .navigationBarBackButtonHidden(true)
+    }
+
+
+    
+    // MARK: - Navigation Bar
+    private var navigationBar: some View {
+        // Navigation Bar
+        HStack(alignment: .top) {
+            Button {
+                dismiss()
+            } label: {
+                Image("arrow_back")
+                    .resizable()
+                    .foregroundStyle(.gray525252)
+                    .frame(width:10.41, height: 17.71)
+            }
+            Spacer()
+            Text("판단 기준 리포트")
+                .font(.PretendardBold16)
+                .foregroundStyle(.gray525252)
+            Spacer()
+        }
+        .padding(.top, 28.37)
+        .padding(.bottom, 21)
+    }
+    
+    // MARK: - Confirm Button
+    private var confirmButton: some View {
+        NavigationLink {
+                ReportLoadingView()
+        } label: {
+            Text("완료")
+                .foregroundStyle(.white)
+                .font(.PretendardBold16)
+                .padding(.vertical,18)
+                .frame(maxWidth:.infinity)
+                .background(Color.accentCoral)
+                .cornerRadius(18)
+        }
+    }
+    
+    // MARK: - Top Content
+    private var topContent: some View {
+        VStack (alignment: .leading){
+            HStack {
+                Text("2025 판단 기준 리포트")
+                    .font(.PretendardBold16)
+                    .foregroundStyle(.gray525252)
+                Spacer()
+                Text("수정 삭제")
+                
+            }
+            Text("ㅇㅇㅇ님은 회피주의자 유형이에요.")
+                .font(.PretendardBold16)
+                .foregroundStyle(.gray525252)
+                .padding(.bottom, 17)
+                .padding(.top, 31)
+        }
+    }
+    
+    // MARK: - Graph Box
+    private var graphBox: some View {
+        ZStack {
+            Rectangle()
+              .foregroundColor(.clear)
+              .frame(height: 164.9)
+              .background(.white)
+              .cornerRadius(17.7)
+              .padding(.bottom, 14.8)
+        }
+    }
+    
+    // MARK: - Middle Content
+    private var middleContent: some View {
+        VStack (alignment: .leading) {
+            
+            Text("2025년 당신의 판단은 \n회피(52%)와 두려움(19%)에서 시작되었습니다.")
+                .font(.PretendardMedium12)
+                .foregroundStyle(.gray525252)
+            
+            Rectangle()
+              .foregroundStyle(.clear)
+              .frame(height: 0.88652)
+              .background(.grayC5C5C5)
+              .padding(.top, 18)
+              .padding(.bottom, 21)
+            
+            Text("가장 반복된 판단 동기")
+                .font(.PretendardBold16)
+                .foregroundStyle(.gray525252)
+                .padding(.bottom, 11.6)
+            Text("Chip Chip")
+            
+            Rectangle()
+              .foregroundStyle(.clear)
+              .frame(height: 0.88652)
+              .background(.grayC5C5C5)
+              .padding(.top, 35)
+              .padding(.bottom, 21)
+            
+            Text("당신의 선택을 가장 많이 이끈 기준")
+                .font(.PretendardBold16)
+                .foregroundStyle(.gray525252)
+                .padding(.bottom, 8.48)
+            
+            Text("2025년 당신의 판단은 \n회피(52%)와 두려움(19%)에서 시작되었습니다.")
+                .font(.PretendardMedium12)
+                .foregroundStyle(.gray525252)
+        }
+    }
+    
+    
+    // MARK: - Writing
+    private var writing: some View {
+        VStack (alignment: .leading){
+            Text("리포트를 기반으로 앞으로의 다짐을 적어보아요.")
+                .font(.PretendardBold16)
+                .foregroundStyle(.gray525252)
+                .padding(.top, 17)
+                .padding(.bottom, 6)
+            
+            Rectangle()
+              .foregroundColor(.clear)
+              .frame(maxWidth: .infinity, minHeight: 98.40406, maxHeight: 98.40406)
+              .background(.white)
+              .cornerRadius(17.73046)
+        }
+    }
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
+#Preview {
+    ReportResultView()
+}
