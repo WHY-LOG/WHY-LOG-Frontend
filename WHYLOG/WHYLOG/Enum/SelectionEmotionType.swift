cb54eccd6742f44620f6db5e861f9693f28f65ff
BeginnerKJS <rlawhdtn10111@gmail.com>
2025-12-19 23:18:44 +0900

    ✨ [Feat] 홈 화면 UI 구현

---
+++ WHYLOG/WHYLOG/Enum/SelectionEmotionType.swift
@@ -0,0 +1,17 @@
+//
+//  11.swift
+//  WHYLOG
+//
+//  Created by 김종수 on 12/16/25.
+//
+
+import Foundation
+
+enum SelectionType: String, CaseIterable {
+    case 비교
+    case 두려움
+    case 기대
+    case 회피
+    case 즉각적만족
+    case 책임감
+}
--- WHYLOG/WHYLOG/Module/Home/HomeView.swift
+++ WHYLOG/WHYLOG/Module/Home/HomeView.swift
@@ -9,21 +9,102 @@ import Foundation
 import SwiftUI
 
 struct HomeView: View {
+    @State private var selectedYear: Int = 2025
+    @State private var selectedMonth: MonthListState = .Mar
+    
+    let months = Array(1...12)
+    
     var body: some View {
         ZStack {
             Color.baseCoral
             .ignoresSafeArea()
-            VStack{
+            VStack(){
                 HStack{
                     Image("WHYLOGLogo")
                         .resizable()
-                        .scaledToFit()
+                        .frame(width: 97, height: 25)
+                    EmotionDropdown()
+                    Spacer()
+                    NavigationLink {
+                        ReportListView()
+                    } label: {
+                        Image(systemName: "text.document")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(.gray525252)
+                    }
+                    NavigationLink {
+                        ReportListView()
+                    } label: {
+                        Image("user")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24, height: 24)
+                            .foregroundColor(.gray525252)
+                    }
                 }
+                HStack(spacing: 20) {
+                    Button(action: { selectedYear -= 1 }) {
+                        Image("arrow_back")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24,height: 24)
+                            .foregroundColor(.gray525252)
+                    }
+                                
+                    Text("\(String(selectedYear))")
+                        .font(.PretendardBold20)
+                                
+                    Button(action: { selectedYear += 1 }) {
+                        Image("arrow_front")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 24,height: 24)
+                            .foregroundColor(.gray525252)
+                    }
+                }.padding(.vertical, 20)
+                            
+                            // 2. 월 선택 (Horizontal Scroll)
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack() {
+                        ForEach(MonthListState.allCases, id: \.self) { month in
+                            MonthButton(state: month,isOn: selectedMonth == month,action: {
+                                    withAnimation(.spring()){
+                                        selectedMonth = month
+                                    }
+                                
+                            })
+                        }
+                    }
+                }
+                Spacer()
+                ScrollView{
+                    VStack(spacing: 16){
+                        ForEach(mockLogs){ recordCardModel in
+                            RecordCard(recordCardModel: recordCardModel)
+                        }
+                    .zIndex(1)
+                    }
+                }
+            }.padding(.horizontal,20)
+            VStack{
+                Spacer()
+                addButton
             }
             
-        }
+        }.navigationBarBackButtonHidden(true)
     }
 }
+
+private var addButton: some View {
+            NavigationLink {
+                CreateRecordView()
+            } label: {
+                AddButtonUI()
+            }.padding(.bottom, 28)
+        }
+
 
 #Preview {
     HomeView()
