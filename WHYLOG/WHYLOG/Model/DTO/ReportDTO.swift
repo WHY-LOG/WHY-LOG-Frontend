cee9e669c0c766769516de27bbc5671c89e860bd
guingguing <kimjinseo0402@gmail.com>
2025-12-21 06:43:20 +0900

    [Feat] ReportDTO 수정

--- WHYLOG/WHYLOG/Model/DTO/ReportDTO.swift
+++ WHYLOG/WHYLOG/Model/DTO/ReportDTO.swift
@@ -7,31 +7,27 @@
 
 import Foundation
 
-// MARK: - 공통 Response
-struct ReportBaseResponse<T: Decodable>: Decodable {
-    let resultType: String
-    let error: String?
-    let success: T
-}
-
-// MARK: - fetchReports
-typealias FetchReportsResponse = ReportBaseResponse<[ReportDTO]>
-
-// MARK: - create / update / delete
-typealias CreateReportResponse = ReportBaseResponse<ReportDTO>
-typealias UpdateReportResponse = ReportBaseResponse<ReportDTO>
-typealias DeleteReportResponse = ReportBaseResponse<EmptyResponse>
-
-// MARK: - Report DTO
+// MARK: - Report List
 struct ReportDTO: Decodable {
     let reportId: Int
-    let title: String
     let year: Int
-    let createdAt: String
 }
 
-// MARK: - Report 생성 / 수정 Request
-struct ReportRequest: Encodable {
-    let title: String
+// MARK: - Report Result
+struct ReportResultDTO: Decodable {
+    let year: Int
+    let standard: String
+    let graphData: [GraphDataDTO]
+}
+
+struct GraphDataDTO: Decodable {
+    let categoryName: String
+    let percent: Int
+    let count: Int?
+}
+
+// MARK: - Request
+struct CreateReportRequest: Encodable {
     let year: Int
 }
+
