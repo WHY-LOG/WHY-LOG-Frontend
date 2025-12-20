d6b4e06605c1f585d80b02def4ea3bf639a13621
bella <bella411@naver.com>
2025-12-21 03:15:47 +0900

    [Feat] RecordDTO 작성 완료

--- WHYLOG/WHYLOG/Model/DTO/RecordDTO.swift
+++ WHYLOG/WHYLOG/Model/DTO/RecordDTO.swift
@@ -7,6 +7,41 @@
 
 import Foundation
 
+// MARK: - Request
+
+//생성 (POST)
+struct CreateRecordRequest: Encodable {
+    let title: String
+    let content: String
+    let categoryIds: [Int]
+    let occurDate: String
+
+    enum CodingKeys: String, CodingKey {
+        case title
+        case content
+        case categoryIds = "category"
+        case occurDate
+    }
+}
+
+//수정 (PUT)
+struct UpdateRecordRequest: Encodable {
+    let title: String
+    let content: String
+    let categoryIds: [Int]
+    let occurDate: String
+
+    enum CodingKeys: String, CodingKey {
+        case title
+        case content
+        case categoryIds = "category"
+        case occurDate
+    }
+}
+
+
+// MARK: - Response
+
 // 공통 Response
 struct RecordBaseResponse<T: Decodable>: Decodable {
     let resultType: String
@@ -14,27 +49,27 @@ struct RecordBaseResponse<T: Decodable>: Decodable {
     let success: T
 }
 
-// fetchRecords 전용 타입 별칭
+// 타입 별칭 정의
 typealias FetchRecordsResponse = RecordBaseResponse<[RecordDTO]>
+typealias CreateRecordResponse = RecordBaseResponse<RecordDTO>
+typealias UpdateRecordResponse = RecordBaseResponse<RecordDTO>
+typealias DeleteRecordResponse = RecordBaseResponse<Int>
 
-// Record
+
+// MARK: - Record DTO
+
 struct RecordDTO: Decodable {
     let recordId: Int
     let title: String
     let content: String
     let occurDate: String
-    let categories: [RecordCategoryDTO]
+    let categories: [String]
 
     enum CodingKeys: String, CodingKey {
-        case recordId = "RecordId"
+        case recordId = "id"
         case title
         case content
         case occurDate
         case categories
     }
-}
-
-struct RecordCategoryDTO: Decodable {
-    let categoryId: Int
-    let categoryName: String
 }
