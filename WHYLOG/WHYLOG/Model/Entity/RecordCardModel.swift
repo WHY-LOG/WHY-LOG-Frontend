90d0af4b17650a60249bc50b1f46c98fbb1b2e1c
BeginnerKJS <rlawhdtn10111@gmail.com>
2025-12-21 07:20:34 +0900

    🐛 [Fix]HomeView UI 및 로직  수정

--- WHYLOG/WHYLOG/Model/Entity/RecordCardModel.swift
+++ WHYLOG/WHYLOG/Model/Entity/RecordCardModel.swift
@@ -9,6 +9,7 @@ import Foundation
 
 struct RecordCardModel: Identifiable {
     let id: UUID = UUID()
+    let year: Int
     let month: String
     let title: String
     let content: String
@@ -16,5 +17,5 @@ struct RecordCardModel: Identifiable {
 }
 
 let mockLogs = [
-    RecordCardModel(month: "3월", title: "싸원던 친구에게 다가가 화해를 하지 못 했다.", content: "사실 별거 아닌 거 그냥 먼저 얘기 꺼냈으면 되는건데, 자존심 때문에 소중한 인연을 놓칠 뻔 했던 것 같다. 다음에는...", emotion: ["두려움","회피"])
+    RecordCardModel(year: 2024, month: "3", title: "싸원던 친구에게 다가가 화해를 하지 못 했다.", content: "사실 별거 아닌 거 그냥 먼저 얘기 꺼냈으면 되는건데, 자존심 때문에 소중한 인연을 놓칠 뻔 했던 것 같다. 다음에는...", emotion: ["두려움","회피"])
 ]
--- WHYLOG/WHYLOG/Module/Report/ReportResult/ReportResultViewModel.swift
+++ WHYLOG/WHYLOG/Module/Report/ReportResult/ReportResultViewModel.swift
@@ -1,65 +1,65 @@
+////
+////  ReportResultViewModel.swift
+////  WHYLOG
+////
+////  Created by 김진서 on 12/20/25.
+////
 //
-//  ReportResultViewModel.swift
-//  WHYLOG
+//import Foundation
+//import Combine
 //
-//  Created by 김진서 on 12/20/25.
+//@MainActor
+//final class ReportResultViewModel: ObservableObject {
 //
-
-import Foundation
-import Combine
-
-@MainActor
-final class ReportResultViewModel: ObservableObject {
-
-    @Published var report: ReportResultDTO?
-    @Published var dominantTypes: [String] = []
-
-    private let reportService = ReportService()
-
-    // MARK: - Load
-    func loadReport(userId: Int, year: Int) async {
-        do {
-            let report = try await reportService.fetchReport(
-                userId: userId,
-                year: year
-            )
-
-            self.report = report
-            calculateDominantTypes(from: report.graphData)
-
-        } catch {
-            print("❌ Failed to load report:", error)
-        }
-    }
-
-    // MARK: - Business Logic
-    private func calculateDominantTypes(from data: [GraphDataDTO]) {
-        let maxPercent = data.map { $0.percent }.max() ?? 0
-
-        dominantTypes = data
-            .filter { $0.percent == maxPercent }
-            .map { $0.categoryName }
-    }
-    
-    // MARK: - Summary Text
-    var summaryText: String {
-        guard let report = report else { return "" }
-
-        let topTwo = report.graphData
-            .sorted { $0.percent > $1.percent }
-            .prefix(2)
-
-        let text = topTwo
-            .map { "\($0.categoryName)(\($0.percent)%)" }
-            .joined(separator: "와 ")
-
-        return "\(report.year)년 당신의 판단은\n\(text)에서 시작되었습니다."
-    }
-
-    // MARK: - AI 답변
-    var standardText: String {
-        report?.standard ?? ""
-    }
-}
-
-
+//    @Published var report: ReportResultDTO?
+//    @Published var dominantTypes: [String] = []
+//
+//    private let reportService = ReportService()
+//
+//    // MARK: - Load
+//    func loadReport(userId: Int, year: Int) async {
+//        do {
+//            let report = try await reportService.fetchReport(
+//                userId: userId,
+//                year: year
+//            )
+//
+//            self.report = report
+//            calculateDominantTypes(from: report.graphData)
+//
+//        } catch {
+//            print("❌ Failed to load report:", error)
+//        }
+//    }
+//
+//    // MARK: - Business Logic
+//    private func calculateDominantTypes(from data: [GraphDataDTO]) {
+//        let maxPercent = data.map { $0.percent }.max() ?? 0
+//
+//        dominantTypes = data
+//            .filter { $0.percent == maxPercent }
+//            .map { $0.categoryName }
+//    }
+//    
+//    // MARK: - Summary Text
+//    var summaryText: String {
+//        guard let report = report else { return "" }
+//
+//        let topTwo = report.graphData
+//            .sorted { $0.percent > $1.percent }
+//            .prefix(2)
+//
+//        let text = topTwo
+//            .map { "\($0.categoryName)(\($0.percent)%)" }
+//            .joined(separator: "와 ")
+//
+//        return "\(report.year)년 당신의 판단은\n\(text)에서 시작되었습니다."
+//    }
+//
+//    // MARK: - AI 답변
+//    var standardText: String {
+//        report?.standard ?? ""
+//    }
+//}
+//
+//
